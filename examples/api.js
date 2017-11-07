b83bb25244c25a5dbc73c48bffba17c7d1ab60a7
rosiecode <rosiecode@outlook.com>
2017-11-07 06:00:41 +0000

    chat start

--- examples/api.js
+++ examples/api.js
@@ -3,4 +3,8 @@ const twitch = require('./../index')('api');
 const api = new twitch({ headers: { 'Client-ID': config.clientID } });
 
 // api.request('GAMES', { name: 'osu!' }).then(console.log).catch(console.error);
-api.request('USERS', { login: 'RosieCode' }).then(console.log).catch(console.error);
+// api.request('USERS', { login: 'RosieCode' }).then(console.log).catch(console.error);
+// api.request('USERS', { id: '53524279' }).then(console.log).catch(console.error);
+
+api.request('USERS_FOLLOWS', { "from_id": 53524279, "to_id": 154508651 }).then(console.log).catch(console.error);
+
---
+++ examples/chat.js
@@ -0,0 +1,3 @@
+const config = require('./config.json');
+const twitch = require('./../index')('chat');
+const chat = new twitch();
