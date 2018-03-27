be3f9a28fb4e7a16848b84c7c3be112fc09aedb2
RosieCode95 <rosiecode@outlook.com>
2018-03-27 23:51:55 +0100

    add support for uws

--- src/chat/index.js
+++ src/chat/index.js
@@ -1,11 +1,8 @@
-const WebSocket = require('ws');
-
 let EventEmitter;
-try {
-    EventEmitter = require('eventemitter3');
-} catch (e) {
-    EventEmitter = require('events').EventEmitter;
-}
+try { EventEmitter = require('eventemitter3'); } catch (e) { EventEmitter = require('events').EventEmitter; }
+
+let WebSocket;
+try { WebSocket = require('uws'); } catch (e) { WebSocket = require('ws'); }
 
 class twitchChat extends EventEmitter {
     constructor(options) {
