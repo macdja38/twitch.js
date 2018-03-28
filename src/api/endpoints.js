5958370226caca5766fa1647f53e744968e03064
RosieCode95 <rosiecode@outlook.com>
2018-03-28 17:39:01 +0100

    updates

--- src/api/endpoints.js
+++
@@ -1,6 +0,0 @@
-module.exports.GAMES = `/games`;
-module.exports.STREAMS = `/streams`;
-module.exports.STREAMS_METADATA = `/streams/metadata`;
-module.exports.USERS = `/users`;
-module.exports.USERS_FOLLOWS = `/users/follows`;
-module.exports.VIDEOS = `/videos`;
--- src/api/index.js
+++ src/api/index.js
@@ -1,32 +1,36 @@
 const snekfetch = require('snekfetch');
-const endpoints = require('./endpoints');
-const constants = require('./constants');
+const ROOT_URL = 'https://api.twitch.tv';
 
 class twitchAPI {
     constructor(options) {
-        this.headers = {
-            Accept: 'application/vnd.twitchtv.v5+json',
-        };
+        this.headers = {};
         Object.assign(this.headers, options.headers);
     }
 
-
-    fetch(endpoint) {
-        return constants.ROOT_URL + endpoints[endpoint];
-    }
-
-    async request(endpoint, headers, params) {
-        this.url = this.fetch(endpoint);
+    async request(url, headers, params) {
         let res;
 
+        const context = {};
+        if (headers) { context.headers = headers; }
+        if (params) { context.query = params; }
+
         try {
-            res = await snekfetch.get(this.url, { headers, query: { params } });
+            res = await snekfetch.get(url, context);
         } catch (error) {
             return error;
         }
 
-        if (res.status !== 200) { throw new Error(res.statusText); }
-        return res.body.data;
+        if (res.status !== 200) { throw new Error(res.statusText, res); }
+        return res.body;
+    }
+
+    getUsers(users) {
+        if (!users) { throw new Error('Missing users paramiter'); }
+        if (!Array.isArray(users)) { throw new Error('Users must be an Array'); }
+        if (users.length === 0) { throw new Error('Users Array must not be empty'); }
+
+        const url = `${ROOT_URL}/kraken/users?login=${users.join(',')}`;
+        return this.request(url, { 'Client-ID': this.headers['Client-ID'], Accept: 'application/vnd.twitchtv.v5+json' });
     }
 }
 
